a1a9736efdd94583a0767bee2fd903aa1ba167e4
Cs.Tamas <csehit@kkszki.hu>
2024-09-24 12:48:48 +0200

    Connect class is created

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +7,33 @@ using System.Threading.Tasks;
 
 namespace Cars
 {
+    public class connect
+    {
+        public static MySqlConnection Connection;
+        private string Host;
+        private string Database;
+        private string Username;
+        private string Password;
+        private string ConnectionString;
+
+        public connect()
+        {
+            Host = "localhost";
+            Database = "cars";
+            Username = "root";
+            Password = "";
+            
+            ConnectionString = "SERVER=" + Host + ";DATABASE=" + Database + ";UID=" + Username + ";PASSWORD=" + Password + ";SslMode=None";
+
+            Connection = new MySqlConnection(ConnectionString);
+        }
+
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
